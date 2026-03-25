681f1425a60f5a9fedd485df99233c8c2e84b289
r-paes <rpaes.0011@gmail.com>
2026-03-24 21:29:49 -0300

    Add files via upload

---
+++ developer-documentation-new-7.0/docs/links/ab_testing.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "A/B testing" 
+link_text = "A/B testing" 
+link_url = "https://kb.mautic.org/article/how-to-create-an-a-b-test-for-emails-in-mautic.html"
+
+link.xref_links.update({link_name: (link_text, link_url)})
---
+++ developer-documentation-new-7.0/docs/links/doctrine_docs_orm.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Doctrine ORM"
+link_text = "Doctrine ORM"
+link_url = "https://www.doctrine-project.org/projects/doctrine-orm/en/2.14/index.html"
+
+link.xref_links.update({link_name: (link_text, link_url)})
